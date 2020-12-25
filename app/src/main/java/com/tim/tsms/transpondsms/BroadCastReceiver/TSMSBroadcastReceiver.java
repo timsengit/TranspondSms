96dfe8a5dae5bcc2384d229f225e91735f0ddda2
tianqing.shen <you@example.com>
2020-12-25 17:02:01 +0800

    fix sms content

--- app/src/main/java/com/tim/tsms/transpondsms/BroadCastReceiver/TSMSBroadcastReceiver.java
+++ app/src/main/java/com/tim/tsms/transpondsms/BroadCastReceiver/TSMSBroadcastReceiver.java
@@ -9,10 +9,11 @@ import android.util.Log;
 import com.tim.tsms.transpondsms.model.vo.SmsVo;
 import com.tim.tsms.transpondsms.utils.SendUtil;
 
-import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.Objects;
 
 public class TSMSBroadcastReceiver  extends BroadcastReceiver {
@@ -26,31 +27,31 @@ public class TSMSBroadcastReceiver  extends BroadcastReceiver {
 
                 Object[] object=(Object[]) Objects.requireNonNull(intent.getExtras()).get("pdus");
                 if(object!=null){
-                    StringBuilder sb=new StringBuilder();
                     List<SmsVo> smsVoList = new ArrayList<>();
                     String format = intent.getStringExtra("format");
-                    SimpleDateFormat simpleDateFormat =new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+                    Map<String,String> mobileToContent=new HashMap<>();
+                    Date date =new Date();
                     for (Object pdus : object) {
                         byte[] pdusMsg=(byte[]) pdus;
                         SmsMessage sms=SmsMessage.createFromPdu(pdusMsg,format);
                         String mobile=sms.getOriginatingAddress();//发送短信的手机号
-                        String content=sms.getMessageBody();//短信内容
+                        if(mobile==null){
+                            continue;
+                        }
                         //下面是获取短信的发送时间
-                        Date date=new Date(sms.getTimestampMillis());
-                        String date_time=simpleDateFormat.format(date);
-                        //追加到StringBuilder中
-                        sb
-                                .append("短信发送号码：")
-                                .append(mobile)
-                                .append("\n短信内容：")
-                                .append(content)
-                                .append("\n发送时间：")
-                                .append(date_time)
-                                .append("\n\n");
-                        smsVoList.add(new SmsVo(mobile,content,date));
+                        date=new Date(sms.getTimestampMillis());
+
+                        String content=mobileToContent.get(mobile);
+                        if(content==null)content="";
+
+                        content+=sms.getMessageBody();//短信内容
+                        mobileToContent.put(mobile,content);
 
                     }
-                    Log.d(TAG,"短信："+sb.toString());
+                    for (String mobile:mobileToContent.keySet()){
+                        smsVoList.add(new SmsVo(mobile,mobileToContent.get(mobile),date));
+                    }
+                    Log.d(TAG,"短信："+smsVoList);
                     SendUtil.send_msg_list(context,smsVoList);
 
                 }
